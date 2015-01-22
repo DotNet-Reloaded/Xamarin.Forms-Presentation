f365c3d86dc6a636999acfd09bfe2344aefd1df7
Carlo Wahlstedt <carlowahlstedt@gmail.com>
2015-01-22 16:51:56 -0600

    Added a BaseViewModel. Had the Login View Model Implement that.

--- PresenationDemo/PresenationDemo/ViewModel/LoginViewModel.cs
+++ PresenationDemo/PresenationDemo/ViewModel/LoginViewModel.cs
@@ -2,11 +2,26 @@
 
 namespace PresenationDemo
 {
-	public class LoginViewModel
+	public class LoginViewModel : BaseViewModel
 	{
-		public string Username { get; set; }
+		string _username;
+		string _password;
 
-		public string Password { get; set; }
+		public string Username {
+			get { return _username; }
+			set {
+				_username = value; 
+				OnPropertyChanged ("username");
+			}
+		}
+
+		public string Password { 
+			get { return _password; }
+			set {
+				_password = value; 
+				OnPropertyChanged ("password");
+			}
+		}
 
 		public Guid Token { get; set; }
 	}
