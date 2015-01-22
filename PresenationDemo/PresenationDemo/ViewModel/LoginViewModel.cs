74c058772e020253c08cb2e8325e81109c27f184
Carlo Wahlstedt <carlowahlstedt@gmail.com>
2015-01-22 16:38:30 -0600

    Added a view model for the login

---
+++ PresenationDemo/PresenationDemo/ViewModel/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PresenationDemo
+{
+	public class LoginViewModel
+	{
+		public string Username { get; set; }
+
+		public string Password { get; set; }
+
+		public Guid Token { get; set; }
+	}
+}
+
