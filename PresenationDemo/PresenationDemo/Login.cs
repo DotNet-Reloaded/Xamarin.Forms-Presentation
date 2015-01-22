9232944b9e89825a623bd16eb259ed27ca0978d1
Carlo Wahlstedt <carlowahlstedt@gmail.com>
2015-01-22 16:02:43 -0600

    Added a login xaml controller and Monkey text

---
+++ PresenationDemo/PresenationDemo/Login.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PresenationDemo
+{
+	public partial class Login : ContentPage
+	{
+		public Login ()
+		{
+			InitializeComponent ();
+		}
+
+		async void OnButtonClicked (object sender, EventArgs args)
+		{
+			await DisplayAlert ("Login", "You succesfully logged in", "Enter Monkey Island");
+			await Navigation.PushAsync (new Monkeys ());
+		}
+	}
+}
+
