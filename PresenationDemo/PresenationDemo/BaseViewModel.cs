0398826c072fb973922f0d9851965457322fcb6d
pbhughes <barkley.hughes@gmail.com>
2015-01-23 13:28:04 -0600

    windows: removed uneeded projects

---
+++ PresenationDemo/PresenationDemo/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace PresenationDemo
+{
+	public class BaseViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void OnPropertyChanged (string name)
+		{
+			if (PropertyChanged == null) {
+				return;
+			}
+
+			PropertyChanged (this, new PropertyChangedEventArgs (name));
+		}
+	}
+}
+
