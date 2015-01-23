54d3fc1fc2b258d0621359c1bbb9f290f4149f00
Carlo Wahlstedt <carlowahlstedt@gmail.com>
2015-01-23 13:19:41 -0600

    Added the class to handle commands and then implemented that on the LoginViewModel.

---
+++ PresenationDemo/PresenationDemo/ViewModel/DelegateCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PresenationDemo
+{
+	public class DelegateCommand : ICommand
+	{
+		Predicate<object> _canExecute { get; set; }
+
+		Action<object> _execute { get; set; }
+
+		public DelegateCommand (Predicate<object> canExecute, Action<object> execute)
+		{
+			_canExecute = canExecute;
+			_execute = execute;
+		}
+
+		#region ICommand implementation
+
+		public event EventHandler CanExecuteChanged;
+
+		public bool CanExecute (object parameter)
+		{
+			return _canExecute == null || _canExecute (parameter);
+		}
+
+		public void Execute (object parameter)
+		{
+			if (_execute != null)
+				_execute (parameter);
+		}
+
+		#endregion
+	}
+}
+
--- PresenationDemo/PresenationDemo/ViewModel/LoginViewModel.cs
+++ PresenationDemo/PresenationDemo/ViewModel/LoginViewModel.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Windows.Input;
 
 namespace PresenationDemo
 {
 	public class LoginViewModel : BaseViewModel
 	{
+		#region Properties
+
 		string _username;
 		string _password;
+		Guid _token;
 
 		public string Username {
 			get { return _username; }
@@ -23,7 +27,34 @@ namespace PresenationDemo
 			}
 		}
 
-		public Guid Token { get; set; }
+		public Guid Token { 
+			get { return _token; }
+			set {
+				_token = value; 
+				OnPropertyChanged ("token");
+			}
+		}
+
+		public ICommand DoLogin { get; set; }
+
+		#endregion
+
+		public LoginViewModel ()
+		{
+			DoLogin = new DelegateCommand (CanLogin, OnLogin);
+		}
+
+		public bool CanLogin (object obj)
+		{
+			return true;
+		}
+
+		public void OnLogin (object obj)
+		{
+			if (CanLogin (null)) {
+				return;
+			}
+		}
 	}
 }
 
