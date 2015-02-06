efaa617255befefa861fb7796b1a48581efd75f7
Carlo Wahlstedt <carlowahlstedt@gmail.com>
2015-02-06 10:26:15 -0500

    Updated Widget screen
    The grid now shows the widget details

--- PresenationDemo/PresenationDemo/ViewModel/WidgetsViewModel.cs
+++ PresenationDemo/PresenationDemo/ViewModel/WidgetsViewModel.cs
@@ -10,36 +10,46 @@ namespace PresenationDemo
 {
 	public class WidgetsViewModel : BaseViewModel
 	{
-		ObservableCollection<Widget> _widgetList = new ObservableCollection<Widget> ();
+		ObservableCollection<Widget> _WidgetList = new ObservableCollection<Widget> ();
+		bool _IsLoading;
 
 		public ObservableCollection<Widget> WidgetList {
-			get { return _widgetList; }
+			get { return _WidgetList; }
 			set {
-				_widgetList = value; 
+				_WidgetList = value; 
 				OnPropertyChanged ("widgetList");
+			}
+		}
+
+		public bool IsLoading {
+			get{ return _IsLoading; }
+			set {
+				_IsLoading = value;
+				OnPropertyChanged ("IsLoading");
 			}
 		}
 
 		public WidgetsViewModel (INavigationService navigation) : base (navigation)
 		{
+
 		}
 
 		public ICommand GetWidgets { 
 			get {
 				return new DelegateCommand (CanGetWidgets, async t => {
+					IsLoading = true;
 					using (var client = new HttpClient ()) {
 						client.BaseAddress = App.WidgetService;
 						client.DefaultRequestHeaders.Add ("Auth", App.TokenBag.Token.ToString ());
 						var response = await client.PostAsync ("Widget/", null);
 						if (response.IsSuccessStatusCode) {
 							var content = await response.Content.ReadAsStringAsync ();
-
 							WidgetList.Clear ();
-
 							var lst = JsonConvert.DeserializeObject<ObservableCollection<Widget>> (content);
 							foreach (var item in lst) {
 								WidgetList.Add (item);
 							}
+							IsLoading = false;
 							await _Navigation.DisplayAlert ("Widgets", "success", "Widget World");
 						}
 					}
